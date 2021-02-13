cea89fd7785d35be7bf3203a92cbb99b1021ad8d
Diogo Musou <diogo.musou@clearwaterclinical.com>
2021-02-13 12:36:40 -0500

    PodTest framework

---
+++ PodTest/PodTest/PodTest.h
@@ -0,0 +1,18 @@
+//
+//  PodTest.h
+//  PodTest
+//
+//  Created by Diogo Musou on 2021-02-13.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for PodTest.
+FOUNDATION_EXPORT double PodTestVersionNumber;
+
+//! Project version string for PodTest.
+FOUNDATION_EXPORT const unsigned char PodTestVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PodTest/PublicHeader.h>
+
+
---
+++ PodTest/PodTest/Service.swift
@@ -0,0 +1,16 @@
+//
+//  Service.swift
+//  PodTest
+//
+//  Created by Diogo Musou on 2021-02-13.
+//
+
+import Foundation
+
+public class Service {
+    private init() {}
+
+    public static func doSomething() -> String {
+        return "Hello Pod World!"
+    }
+}
