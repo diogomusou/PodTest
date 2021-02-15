719c7db02e70631683b5bb85a43cde1cbfc03ab9
Diogo Musou <diogo.musou@clearwaterclinical.com>
2021-02-15 17:33:35 -0500

    it worked

--- Sources/PodTest/Service.swift
+++ Sources/PodTest/Service.swift
@@ -11,15 +11,6 @@ public class Service {
     private init() {}
 
     public static func doSomething() -> String {
-//        let thisBundle = Bundle(for: Self.self)
-//        guard let bundlePath = thisBundle.url(forResource: "PodTest", withExtension: "bundle") else {
-//            fatalError("no path")
-//        }
-//
-//        guard let bundle = Bundle(url: bundlePath) else {
-//            fatalError("no bundle")
-//        }
-        return L10nAdmin.Passcode.Title.enterPasscode
-//        return NSLocalizedString("Passcode.Title.enterPasscode", tableName: "Localizable", bundle: bundle ,value: "oops", comment: "something")
+        L10nAdmin.Passcode.Title.enterPasscode
     }
 }
